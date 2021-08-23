7fce83019f0e2e830d97ae0678f62f023a254227
Shivesh Navin <shivesh.navin@sap.com>
2021-08-23 12:33:04 +0530

    backend done

--- src/main/java/com/sap/cf/sample/repository/ScholarRepo.java
+++ src/main/java/com/sap/cf/sample/repository/ScholarRepo.java
@@ -4,9 +4,7 @@ import com.sap.cf.sample.dao.Scholar;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface ScholarRepo extends JpaRepository<Scholar, UUID> {
+public interface ScholarRepo extends JpaRepository<Scholar, Long> {
 
 }
